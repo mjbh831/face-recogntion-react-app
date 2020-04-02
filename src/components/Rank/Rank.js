1fb72b1fe6796b35bdad6921879f10294d4a02dc
mjbh831 <56654912+mjbh831@users.noreply.github.com>
2020-04-02 10:03:53 -0500

    added code to connect to server

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,15 +1,15 @@
 import React from 'react';
  
-const Rank = () => {
+const Rank = ({name, entries}) => {
 
 	return (
 		<div className=''>	
 
 		<div className='white f3'>
-			{'Marcos, your current rank is...'}
+			{`${name}, your current entry count is...`}
 		</div>
 		<div className='white f1'>
-			{'#5'}
+			{`${entries}`}
 		</div>
 			
 				
